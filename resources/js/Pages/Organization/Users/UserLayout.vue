92d6c4885ceb20832d2acfe9264d5171ffd4c0cd
Curtis Werstuik <cwerstuik@localhost.localdomain>
2026-04-25 20:47:25 -0700

    Fix mobile UI layout issues

--- resources/js/Pages/Organization/Users/UserLayout.vue
+++ resources/js/Pages/Organization/Users/UserLayout.vue
@@ -3,23 +3,17 @@ import { Link } from '@inertiajs/vue3'
 
 </script>
 <template>
-  <div class="user">
-    <div class="row">
-      <div class="flex xs12 lg12">
-        <va-card class="mb-4">
-          <va-card-content>
-            <va-tabs v-model="value" hide-slider>
-              <template #tabs>
-                <Link v-for="(tab, index) in tabs" :href="tab.url" :key="index"><va-tab :key="tab.title" :name="tab.title">{{ tab.title }}</va-tab></Link>
-              </template>
-            </va-tabs>
-            <va-separator />
-            <slot></slot>
-          </va-card-content>
-        </va-card>
-      </div>
-    </div>
-  </div>
+  <va-card class="mb-4">
+    <va-card-content>
+      <va-tabs v-model="value" hide-slider>
+        <template #tabs>
+          <Link v-for="(tab, index) in tabs" :href="tab.url" :key="index"><va-tab :key="tab.title" :name="tab.title">{{ tab.title }}</va-tab></Link>
+        </template>
+      </va-tabs>
+      <va-separator />
+      <slot></slot>
+    </va-card-content>
+  </va-card>
 </template>
 
 <script lang="ts">
