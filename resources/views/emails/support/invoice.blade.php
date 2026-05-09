4f68559301c4b0f6ad56aa8b5ce32998b17b4a34
cwerstuik <geekatron101@gmail.com>
2026-05-08 20:46:24 -0700

    fix some mispelt translation strings
    Signed-off-by: cwerstuik <geekatron101@gmail.com>

--- resources/views/emails/support/invoice.blade.php
+++ resources/views/emails/support/invoice.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
-<p>{{ ('messages.notifications.invoice.line_1') }}: {{ $description }}</p>
-<p>{{ ('messages.notifications.invoice.line_2') }} <a href="{{ env('APP_URL') }}">{{ env('APP_URL') }}</a></p>
-<p>{{ ('messages.notifications.invoice.line_3') }}:</p>
+<p>{{ ('messages.notification.invoice.line_1') }}: {{ $description }}</p>
+<p>{{ ('messages.notification.invoice.line_2') }} <a href="{{ env('APP_URL') }}">{{ env('APP_URL') }}</a></p>
+<p>{{ ('messages.notification.invoice.line_3') }}:</p>
 <p>@foreach($admins as $admin){{ $admin->attribute('first_name') }} {{ $admin->attribute('last_name') }}, @endforeach</p>
 @endcomponent
