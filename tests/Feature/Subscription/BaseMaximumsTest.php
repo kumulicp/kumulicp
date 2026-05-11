a178a6cef0bc919f77649338af848a6d1abec1af
cwerstuik <geekatron101@gmail.com>
2026-05-10 20:56:24 -0700

    move email tests to their own folder. Minor fixes
    Signed-off-by: cwerstuik <geekatron101@gmail.com>

--- tests/Feature/Subscription/BaseMaximumsTest.php
+++ tests/Feature/Subscription/BaseMaximumsTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Subscription;
 
 use App\Support\Facades\AccountManager;
-use App\Support\Facades\Subscription;
 
 class BaseMaximumsTest extends SubscriptionTestCase
 {
@@ -99,51 +98,11 @@ class BaseMaximumsTest extends SubscriptionTestCase
         $domain1 = $this->post('/settings/domains/connect', ['domain_name' => 'example1.com']);
         $domain1->assertSee('example1.com');
 
+        $this->followingRedirects();
         $domain2 = $this->post('/settings/domains/connect', ['domain_name' => 'example2.com']);
         $domain2->assertSee('example2.com');
 
         $domain3 = $this->post('/settings/domains/connect', ['domain_name' => 'example3.com']);
-        $domain3->assertStatus(403);
-    }
-
-    public function test_max_emails_reached()
-    {
-        $this->followingRedirects();
-        $this->user->organization->domains()->update(['email_enabled' => true]);
-
-        $emailServer = $this->createEmailServer();
-        $this->support->base_2->email_enabled = true;
-        $this->support->base_2->email_server_id = $emailServer->id;
-        $this->support->base_2->save();
-
-        $this->support->setSubscription($this->user->organization, $this->support->base_2);
-        Subscription::refresh();
-
-        $domain1 = $this->post('/settings/email/accounts', [
-            'name' => 'test1',
-            'email' => 'test1',
-            'domain' => $this->user->organization->domains()->first()->id,
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-        $domain1->assertSee('test1');
-
-        $domain2 = $this->post('/settings/email/accounts', [
-            'name' => 'test2',
-            'email' => 'test2',
-            'domain' => $this->user->organization->domains()->first()->id,
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-        $domain2->assertSee('test2');
-
-        $domain3 = $this->post('/settings/email/accounts', [
-            'name' => 'test3',
-            'email' => 'test3',
-            'domain' => $this->user->organization->domains()->first()->id,
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
         $domain3->assertStatus(403);
     }
 }
