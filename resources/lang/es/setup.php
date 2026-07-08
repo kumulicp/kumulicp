7d3b3104c5c850047bc88507ccff3cb1fd30f02b
cwerstuik <geekatron101@gmail.com>
2026-07-08 12:38:27 -0700

    Add AI spanish translation

---
+++ resources/lang/es/setup.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'extension' => 'Configuración de PHP: Extensión cargada :extension',
+    'function' => 'Configuración de PHP: Función habilitada: :function',
+    'stripe_connection' => 'Conexión con Stripe',
+    'plan' => [
+        'name' => 'El Plan Definitivo',
+        'description' => '¡Aplicaciones, almacenamiento y tráfico ilimitados de forma gratuita!',
+    ],
+    'complete' => 'El panel de control se ha configurado correctamente',
+    'first_org' => 'Cuenta de súper administrador',
+];
