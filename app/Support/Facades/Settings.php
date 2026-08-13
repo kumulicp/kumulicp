c27a99021832af040b4da0d8a22379a640ca581d
cwerstuik <geekatron101@gmail.com>
2026-08-12 20:40:01 -0700

    add phpstan doc comments

--- app/Support/Facades/Settings.php
+++ app/Support/Facades/Settings.php
@@ -4,6 +4,13 @@ namespace App\Support\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static mixed get(string $key, mixed $fallback = '')
+ * @method static void update(string $key, mixed $value = null)
+ * @method static void remove(string $key)
+ *
+ * @see \App\Services\SettingsService
+ */
 class Settings extends Facade
 {
     /**
