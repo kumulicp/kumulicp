ff6658c60be46c02138cef170ac2025c0cd843fe
cwerstuik <geekatron101@gmail.com>
2026-08-25 18:11:34 -0700

    merge routes from last rebase

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Scripts -->
-        @routes(group: \App\Support\Ziggy::groupFor(auth()->user()))
-        @routes(nonce: \Illuminate\Support\Facades\Vite::cspNonce())
+        @routes(group: \App\Support\Ziggy::groupFor(auth()->user()), nonce: \Illuminate\Support\Facades\Vite::cspNonce())
         @vite('resources/js/app.js')
         @inertiaHead
     </head>
