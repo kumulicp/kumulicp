ac639fcc1e1d4a0b2a48cbcf4825ba2336e8d604
cwerstuik <geekatron101@gmail.com>
2026-09-10 21:12:34 -0700

    add appinstance to activationtype method
    Signed-off-by: cwerstuik <geekatron101@gmail.com>

--- app/Integrations/ServerManagers/HelmKubernetes/Interfaces/HelmKubernetesWebInterface.php
+++ app/Integrations/ServerManagers/HelmKubernetes/Interfaces/HelmKubernetesWebInterface.php
@@ -61,7 +61,7 @@ class HelmKubernetesWebInterface implements AppInterface, OrganizationInterface
     {
         $app = new Application($this->organization, $this->server);
 
-        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType() === 'job') {
+        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType($this->app_instance) === 'job') {
             $charts = ApplicationFacade::instance($this->app_instance->parent)->charts();
         } else {
             $charts = ApplicationFacade::instance($this->app_instance)->charts();
@@ -86,7 +86,7 @@ class HelmKubernetesWebInterface implements AppInterface, OrganizationInterface
     public function get()
     {
         $app_instance = $this->app_instance;
-        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType() === 'job') {
+        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType($this->app_instance) === 'job') {
             $app_instance = $this->app_instance->parent;
             $charts = ApplicationFacade::instance($app_instance)->charts();
         } else {
@@ -142,7 +142,7 @@ class HelmKubernetesWebInterface implements AppInterface, OrganizationInterface
 
     private function chartsForStatus(): array
     {
-        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType() === 'job') {
+        if (ApplicationFacade::profile($this->app_instance->application->slug)->activationType($this->app_instance) === 'job') {
             return ApplicationFacade::instance($this->app_instance->parent)->charts();
         }
 
